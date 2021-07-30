e6246b0ba7be55396c011186bde80a3972966b9b
Vishnu <vishnuvijayakumar125@gmail.com>
2021-07-30 19:25:31 +1000

    test

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+const knex = require('knex');
+const { now } = require('tarn/dist/utils');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+const postgres = knex ({
+    client: 'postgres',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'dashboard'
+    }
+  });
+
+app.use(cors())
+
+
+app.get('/',(req,res)=>{
+    
+    postgres.select().table('carddata').then((data)=>
+{
+    res.send(data)
+})
+
+}
+)
+
+app.get('/livedata',(req,res)=>{
+    
+    postgres.select().table('livedata').then((data)=>
+{
+    res.send(data)
+})
+})
+
+app.get('/:name',(req,res)=>{
+    
+    let value = (req.params.name)
+    value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+    console.log(value)
+    postgres.select().table('carddata').where('process',value).then((data)=>
+{
+    res.send(data)
+})
+})
+
+
+app.put('/carddata/:process',(req,res)=>{
+    
+    // postgres('carddata').update({name:'bottom'}).then(d=>console.log(d))
+    // console.log('post')
+    console.log('put')
+    data = req.body
+    p = req.params.process
+    p = p.charAt(0).toUpperCase() + p.slice(1).toLowerCase()
+    // // postgres('carddata').where({"value":"FIM"}).update({'name':'top'}).then(d => console.log(d))
+    if (p == 'Application')
+    {
+        postgres('carddata').where({value:data['value']}).update({name:data['name'],lastupdated:'now()'},['name','value']).then(data => res.send(data))
+    }
+    else
+    {
+        console.log(p)
+        postgres('carddata').where({name:data['name'],process:p}).update({value:data['value'],lastupdated:'now()'},['name','value']).then(data => res.send(data))
+    }
+
+
+})
+
+
+
+// app.put('/livedata/:process',(req,res)=>{
+    
+//     // postgres('carddata').update({name:'bottom'}).then(d=>console.log(d))
+//     // console.log('post')
+//     console.log('put')
+//     data = req.body
+//     p = req.params.process
+//     p = p.charAt(0).toUpperCase() + p.slice(1).toLowerCase()
+//     // // postgres('carddata').where({"value":"FIM"}).update({'name':'top'}).then(d => console.log(d))
+//     if (p == 'Application')
+//     {
+//         console.log(now())
+//         postgres('carddata').where({value:data['value']}).update({name:data['name'],lastupdated:now()},['name','value']).then(data => res.send(data))
+//     }
+
+
+// })
+app.listen(4000);
